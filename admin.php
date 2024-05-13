e547ac83dfe198edcc7e5162297433478a5b6e98
Tia Bishwakarma <118331720+tia-01@users.noreply.github.com>
2024-05-13 12:49:00 +0545

    Update admin.php

--- admin.php
+++ admin.php
@@ -153,7 +153,7 @@ border-radius: 15px;
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
             </svg> 
-            <a href="http://172.162.20.155:5500/project%20II/myprofile.html" style="text-decoration:none; color:inherit;">  <i class="bi bi-person-circle">My Profile</i> </a>
+            <a href="prof.html" style="text-decoration:none; color:inherit;">  <i class="bi bi-person-circle">My Profile</i> </a>
         </button>
     </div>
     </nav>
